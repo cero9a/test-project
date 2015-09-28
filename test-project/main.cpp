d32c2472bb4645322c4c6d0aae6cf8019d2749e2
cero9a <roiksergiy@mail.ru>
2015-09-29 02:05:44 +0300

    added project files

--- test-project/main.cpp
+++ test-project/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int dan;
+    cout << "Vvedit kamin(1) nozn(2) papir(3): ";
+    cin >> dan;
+    cout << "Vu vvelu: " << dan << '\n';
+    int kom;
+    kom = rand()%3;
+    cout << kom << '\n';
+    if (dan ==kom)
+        cout << "friend";
+    if (dan > kom)
+        cout << "looser";
+    if (dan < kom)
+        cout << "winner";
     return 0;
 }
 
